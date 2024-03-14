bf80ef6e5217df26b7b51e4e6c985e14bd6fdace
Sahil Jain <96883325+sjain1810@users.noreply.github.com>
2024-03-14 18:28:38 +0000

    arrays part1 code

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+//  array
+
+const myArr = [0, 1, 2, 3, 4]
+
+//  myArr.forEach((item) => console.log(item))
+// console.log(myArr.reduce((item, item1) => item - item1))
+// myArr.filter((item) => item > 2 ? console.log(item) : -1)
+// console.log(myArr.map(Math.sqrt))
+
+const arr =  ["Sahil", "Jain"]
+const arr1 = new Array(1,2,3,4,5)
+// console.log(arr1);
+// console.log(arr1[0]);
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.shift() // removes elem from beg
+// myArr.unshift(9) // adds elem to beg
+
+// console.log(myArr);
+
+// console.log(myArr.includes(0));
+// console.log(myArr.indexOf(0));
+
+// const arr3 = myArr.join() // O/P :- 0,1,2,3,4
+// const arr3 = myArr.join("-") // O/P :- 0-1-2-3-4
+// console.log(arr3);
+
+// slice and spice 
+
+const arr4 = [0,1,2,3,4]
+
+// console.log("A: ",arr4)
+
+const arr5 = arr4.slice(0,3)
+// console.log("B: ", arr5)
+
+const arr6 = arr4.splice(0,3)
+// console.log("C: ",arr4);
+// console.log(arr6)
