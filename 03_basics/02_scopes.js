9c0739df9306aee8d9b5020395de422fb948b252
Sahil Jain <96883325+sjain1810@users.noreply.github.com>
2024-03-19 17:02:11 +0000

    scope level and mini hoisting code

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,4 +9,42 @@ if(true){
 
 var a = 100
 
-console.log(a);
+// console.log(a);
+
+function one(){
+    const username = "Sahil"
+    function two(){
+        const website = "sj.com"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Sahil"
+    if(username === "Sahil"){
+        const website = "sj.com"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ***************Interesting*****************
+
+
+one(5)
+function one(num){
+    return num + 1
+}
+
+
+
+two() // We get error in this case, therefore we can see that executing expression, i.e.,
+     //  constant holding  function before initialization throws error
+const two = function(num){
+    return num + 1
+}
